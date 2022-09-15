9e6a330ec8556944b5b7ebaf633f3f2bc71adaf0
apkrit <apkritikos@gmail.com>
2022-09-15 06:04:59 +0000

    Adding gtag

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GoogleAnalytics />
     <Nav />
     <App />
 
