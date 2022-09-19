02d40a129454e5635ac63823bba536c972fda876
apkrit <apkritikos@gmail.com>
2022-09-19 07:15:48 +0000

    Fixed responsiveness of cartoons on landing page

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -15,30 +15,30 @@ function Home () {
         <div className="App">
             <Nav />
             <div className="NameBar">
-            <h1>Houzey</h1>
+                <h1>Houzey</h1>
             </div>
 
             <header className="App-header">
-            <img src={logo} className="Home-logo" alt="logo" />
+                <img src={logo} className="Home-logo" alt="logo" />
             </header>
 
             <body className="App-body">
-            <h1>Everyone likes a nicely decorated home... 
-                <br></br>but it's not always so easy.</h1>
-            <img src={cartoon1} className="Cartoon" alt="cartoon" />
-            <img src={cartoon1v2} className="Cartoon" alt="cartoon" />
-            <img src={cartoon2} className="Cartoon" alt="cartoon" />
-            <img src={cartoon3} className="Img-rotate" alt="cartoon" />
-            <img src={cartoon4} className="Cartoon" alt="cartoon" />
-            <img src={cartoon5} className="Cartoon" alt="cartoon" />
+                <h1>Everyone likes a nicely decorated home... 
+                    <br></br>but it's not always so easy.</h1>
+                <img src={cartoon1} className="Cartoon" alt="cartoon" />
+                <img src={cartoon1v2} className="Cartoon" alt="cartoon" />
+                <img src={cartoon2} className="Cartoon" alt="cartoon" />
+                <img src={cartoon3} className="Img-rotate" alt="cartoon" />
+                <img src={cartoon4} className="Cartoon" alt="cartoon" />
+                <img src={cartoon5} className="Cartoon" alt="cartoon" />
             </body>
 
             <body className="App-body">
-            <h1>Let our Houzey Helpers, help you.</h1>
-            <h2>Use our website or app to purchase or rent decorations.</h2>
-            <h2>Schedule a time that works for you.</h2>
-            <h2>Then, just wait for our Houzey Helpers to show up.</h2>
-            <h2>Watch our team do everything - from installation to takedown.</h2>
+                <h1>Let our Houzey Helpers, help you.</h1>
+                <h2>Use our website or app to purchase or rent decorations.</h2>
+                <h2>Schedule a time that works for you.</h2>
+                <h2>Then, just wait for our Houzey Helpers to show up.</h2>
+                <h2>Watch our team do everything - from installation to takedown.</h2>
             </body>
 
             <Footer />
