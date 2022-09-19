76a6e4d8437cbaabd2624818fae65015e2e59233
apkrit <apkritikos@gmail.com>
2022-09-19 18:55:02 +0000

    Adjusted styling for LandingPage page text

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -35,10 +35,12 @@ function Home () {
 
             <body className="App-body">
                 <h1>Let our Houzey Helpers, help you.</h1>
-                <h2>Use our website or app to purchase or rent decorations.</h2>
-                <h2>Schedule a time that works for you.</h2>
-                <h2>Then, just wait for our Houzey Helpers to show up.</h2>
-                <h2>Watch our team do everything - from installation to takedown.</h2>
+                <div className="BulletinList">
+                    <h3>Use our website or app to purchase or rent decorations.</h3>
+                    <h3>Schedule a time that works for you.</h3>
+                    <h3>Then, just wait for our Houzey Helpers to show up.</h3>
+                    <h3>Watch our team do everything - from installation to takedown.</h3>
+                </div>
             </body>
 
             <Footer />
