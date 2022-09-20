b02efa901686f82024b51ade9b1df00c47cfa7c7
apkrit <apkritikos@gmail.com>
2022-09-20 16:27:40 +0000

    Updated guest form to use Select field

--- src/Components/GuestZipForm.js
+++ src/Components/GuestZipForm.js
@@ -2,7 +2,12 @@ import '../Styles/main.css';
 import { useState } from "react";
 
 const GuestZipForm = () => {
-  const [title, setTitle] = useState('');
+  const [guestZip, setGuestZip] = useState('');
+
+  const handleGuestZipFormSubmit = (e) => {
+    e.preventDefault();
+  }
+
     return (
         <main>
 
@@ -12,23 +17,27 @@ const GuestZipForm = () => {
                 
                 <div className="GuestBackgroundWrapper">
 
-                  <h3>Enter a valid city to start browsing<br></br>(e.g., Salem or 01970)</h3>
-                  <h3>Or keep scrolling to learn a little more</h3>
                   <h2>Currently Serving:<br></br>Salem, Peabody, Lynn</h2>
+                  <h3>Select a city to start browsing</h3>
+                  <h3>Or keep scrolling to learn a little more</h3>
+                  
                   <p>More locations coming soon!</p>
                   
-                  <div className="GuestZipFormInput">           
-                    <form>
-                      <label></label>
-                      <input 
-                        type="text"
-                        placeholder="Enter City or Zip Code"
-                        required
-                        value={title}
-                        onChange={(e) => setTitle(e.target.value)}
-                      />
+                  <div className="GuestZipFormInput">     
+
+                    <form onSubmit={handleGuestZipFormSubmit}>                               
+                      <select
+                      value={guestZip}
+                      onChange={(e) => setGuestZip(e.target.value) }
+                      >
+                        <option value="">Select City</option>
+                        <option value="Salem">Salem</option>
+                        <option value="Peabody">Peabody</option>
+                        <option value="Lynn">Lynn</option>
+                      </select> 
                       <button className="Button">GO</button>
                     </form>
+
                   </div>
 
                   <p>Sign in to use your stored home address</p>
